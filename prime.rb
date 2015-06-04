a642209355b952282af4702b3d24f7dff09033ea
elainapolson <elainapolson@gmail.com>
2015-06-04 14:01:06 -0400

    done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,42 @@
-# Add  code here!
+def prime?(number)
+  counter = 2
+  prime_or_not = true
+  while counter < number
+    if number % counter == 0
+      prime_or_not = false
+    end 
+    counter += 1
+  end 
+  prime_or_not
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def prime?(num)
+#   (2...num).each do |divisor|
+#     return false if num % divisor == 0
+#   end
+
+#   true
+# end
